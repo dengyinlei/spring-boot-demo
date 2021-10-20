2df88149bf0cf288a09a820fd2bf58e6ff420272
denley <936489270@qq.com>
2021-10-20 19:44:33 +0800

    新增ffmpeg demo

---
+++ demo-ffmpeg/src/main/java/com/example/ffmpeg/DemoFfmpegApplication.java
@@ -0,0 +1,13 @@
+package com.example.ffmpeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFfmpegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFfmpegApplication.class, args);
+    }
+
+}
