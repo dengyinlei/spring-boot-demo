81d98faae650f43e4e557ad218cd539651504f2b
denley <936489270@qq.com>
2021-12-21 19:18:43 +0800

    新增ffmpeg demo

---
+++ demo-mq-kafka/src/main/java/com/xkcoding/mq/kafka/config/TopicAdministrator.java
@@ -0,0 +1,30 @@
+package com.xkcoding.mq.kafka.config;
+
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Configuration
+public class TopicAdministrator {
+
+    private final TopicConfigurations configurations;
+    private final GenericWebApplicationContext context;
+
+    public TopicAdministrator(TopicConfigurations configurations, GenericWebApplicationContext genericContext) {
+        this.configurations = configurations;
+        this.context = genericContext;
+    }
+
+    @PostConstruct
+    public void init() {
+        initializeBeans(configurations.getTopics());
+    }
+
+    private void initializeBeans(List<TopicConfigurations.Topic> topics) {
+        topics.forEach(t -> context.registerBean(t.name, NewTopic.class, t::toNewTopic));
+    }
+}
